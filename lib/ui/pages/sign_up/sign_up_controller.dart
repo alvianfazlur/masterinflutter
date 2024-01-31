7d902f09e1ba1d1827c82d5c5a7bf32b809b2b18
alvianfazlur <m.alvianfazlurfd16@gmail.com>
2024-01-31 11:06:03 +0700

    sign up

--- lib/ui/pages/sign_up/sign_up_controller.dart
+++ lib/ui/pages/sign_up/sign_up_controller.dart
@@ -4,57 +4,41 @@ import 'package:equatable/equatable.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 
-class SignUpController extends GetxController with EquatableMixin {
+enum AuthStatus { loading, success, failed }
 
+class SignUpController extends GetxController with EquatableMixin {
+  Rx<AuthStatus> status = AuthStatus.loading.obs;
   TextEditingController nameController = TextEditingController();
   TextEditingController emailController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
   TextEditingController hobbyController = TextEditingController();
 
-  @override
-  void onInit() {
-    super.onInit();
-  }
+  userData? user;
+  String? error;
 
-  Future<void> signup(
-      {required String email,
-      required String password,
-      required String name,
-      String hobby = ""}) async {
+  Future<void> signup({
+    required String email,
+    required String password,
+    required String name,
+    String hobby = "",
+  }) async {
     try {
-      AuthLoading();
-      update();
+      user = await AuthService().signUp(
+        email: email,
+        password: password,
+        name: name,
+        hobby: hobby,
+      );
 
-      userData user = await AuthService()
-          .signUp(email: email, password: password, name: name, hobby: hobby);
-      AuthSuccess(user);
+      status = AuthStatus.success.obs;
       update();
     } catch (e) {
-      AuthFailed(e.toString());
+      status = AuthStatus.failed.obs;
+      error = e.toString();
+      update();
     }
   }
 
   @override
-  List<Object?> get props => [];
-}
-
-class AuthLoading extends SignUpController {}
-
-class AuthSuccess extends SignUpController {
-  final userData user;
-
-  AuthSuccess(this.user);
-
-  @override
-  List<Object?> get props => [user];
-}
-
-class AuthFailed extends SignUpController {
-  final String error;
-
-  AuthFailed(this.error);
-
-  @override
-  // TODO: implement props
-  List<Object?> get props => [error];
+  List<Object?> get props => [status, user, error];
 }
--- lib/ui/widgets/input_section.dart
+++ lib/ui/widgets/input_section.dart
@@ -5,123 +5,129 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class InputSection extends StatelessWidget {
-  var controller = Get.find<SignUpController>();
+  // var controller = Get.find<SignUpController>();
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.only(top: 30),
-      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 30),
-      child: Column(
-        children: [
-          Container(
-            margin: EdgeInsets.only(bottom: 20),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text("Full Name"),
-                SizedBox(
-                  height: 6,
-                ),
-                TextFormField(
-                  controller: controller.nameController,
-                  decoration: InputDecoration(
-                      hintText: "Masukkan Nama Lengkap",
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(defaultRadius)
-                      ),
-                    focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(defaultRadius),
-                      borderSide: BorderSide(
-                        color: primaryColor
-                      )
-                    )
+    return GetBuilder<SignUpController>(
+      builder: (controller) => Container(
+        margin: EdgeInsets.only(top: 30),
+        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 30),
+        child: Column(
+          children: [
+            Container(
+              margin: EdgeInsets.only(bottom: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text("Full Name"),
+                  SizedBox(
+                    height: 6,
                   ),
-                ),
-                SizedBox(height: 20,),
-                Text("Email Address"),
-                SizedBox(
-                  height: 6,
-                ),
-                TextFormField(
-                  controller: controller.emailController,
-                  decoration: InputDecoration(
-                      hintText: "Masukkan Email",
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(defaultRadius)
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(defaultRadius),
-                          borderSide: BorderSide(
-                              color: primaryColor
-                          )
-                      )
+                  TextFormField(
+                    controller: controller.nameController,
+                    decoration: InputDecoration(
+                        hintText: "Masukkan Nama Lengkap",
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(defaultRadius)),
+                        focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(defaultRadius),
+                            borderSide: BorderSide(color: primaryColor))),
                   ),
-                ),
-                SizedBox(height: 20,),
-                Text("Password"),
-                SizedBox(
-                  height: 6,
-                ),
-                TextFormField(
-                  controller: controller.passwordController,
-                  obscureText: true,
-                  decoration: InputDecoration(
-                      hintText: "Masukkan Password",
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(defaultRadius)
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(defaultRadius),
-                          borderSide: BorderSide(
-                              color: primaryColor
-                          )
-                      )
+                  SizedBox(
+                    height: 20,
                   ),
-                ),
-                SizedBox(height: 20,),
-                Text("Hobby"),
-                SizedBox(
-                  height: 6,
-                ),
-                TextFormField(
-                  controller: controller.hobbyController,
-                  decoration: InputDecoration(
-                      hintText: "Hobby",
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(defaultRadius)
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(defaultRadius),
-                          borderSide: BorderSide(
-                              color: primaryColor
-                          )
-                      )
+                  Text("Email Address"),
+                  SizedBox(
+                    height: 6,
                   ),
-                ),
-                SizedBox(height: 30,),
-                Container(
-                  width: double.infinity,
-                  height: 55,
-                  child: TextButton(
-                    style: TextButton.styleFrom(
-                        backgroundColor: primaryColor,
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(defaultRadius))),
-                    onPressed: () {
-                      controller.signup(email: controller.emailController.text, password: controller.passwordController.text, name: controller.nameController.text, hobby: controller.hobbyController.text);
-                    },
-                    child: Text(
-                      'Get Started',
-                      style: whiteTextStyle.copyWith(
-                          fontWeight: medium, fontSize: 18),
+                  TextFormField(
+                    controller: controller.emailController,
+                    decoration: InputDecoration(
+                        hintText: "Masukkan Email",
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(defaultRadius)),
+                        focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(defaultRadius),
+                            borderSide: BorderSide(color: primaryColor))),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Text("Password"),
+                  SizedBox(
+                    height: 6,
+                  ),
+                  TextFormField(
+                    controller: controller.passwordController,
+                    obscureText: true,
+                    decoration: InputDecoration(
+                        hintText: "Masukkan Password",
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(defaultRadius)),
+                        focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(defaultRadius),
+                            borderSide: BorderSide(color: primaryColor))),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Text("Hobby"),
+                  SizedBox(
+                    height: 6,
+                  ),
+                  TextFormField(
+                    controller: controller.hobbyController,
+                    decoration: InputDecoration(
+                        hintText: "Hobby",
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(defaultRadius)),
+                        focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(defaultRadius),
+                            borderSide: BorderSide(color: primaryColor))),
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  Container(
+                    width: double.infinity,
+                    height: 55,
+                    child: TextButton(
+                      style: TextButton.styleFrom(
+                          backgroundColor: primaryColor,
+                          shape: RoundedRectangleBorder(
+                              borderRadius:
+                                  BorderRadius.circular(defaultRadius))),
+                      onPressed: () async {
+                        await controller.signup(
+                            email: controller.emailController.text,
+                            password: controller.passwordController.text,
+                            name: controller.nameController.text,
+                            hobby: controller.hobbyController.text);
+                        if (controller.status == AuthStatus.loading) {
+                          print("Loadinggg");
+                        } else if (controller.status == AuthStatus.success) {
+                          print("Success");
+                          Get.offNamed(BonusPage.routeName);
+                        } else if (controller.status == AuthStatus.failed) {
+                          print("Failed");
+                          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                              content:
+                                  Text(controller.error ?? 'Unknown error')));
+                        }
+                      },
+                      child: Text(
+                        'Get Started',
+                        style: whiteTextStyle.copyWith(
+                            fontWeight: medium, fontSize: 18),
+                      ),
                     ),
-                  ),
-                )
-              ],
+                  )
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
